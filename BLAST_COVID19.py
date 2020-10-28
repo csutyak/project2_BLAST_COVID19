f2c5ea59a599bd8d9d071a9cd865e7a28721186d
csutyak <cps46@zips.uakron.edu>
2020-10-28 16:43:57 -0400

    add fileToString function

--- BLAST_COVID19.py
+++ BLAST_COVID19.py
@@ -1,3 +1,15 @@
 import os
 
-print("Hello")
+SARS_N_INPUT_FILENAME = "SARS_N.txt"
+
+def fileToString(filename):
+	inputFile = open(filename, "r")
+	inputlines = inputFile.readlines()
+	CompleteString = ""
+	for line in inputlines:
+		workingString = line.replace(" ", "")
+		workingString = workingString.replace("\n", "")
+		CompleteString = CompleteString + workingString
+	return CompleteString
+
+print(fileToString(SARS_N_INPUT_FILENAME))
